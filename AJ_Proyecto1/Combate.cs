d5cac18a180ba6b720a62f82fd54f64f2c187f2f
Alejandro Hernández Calero <alehdzclr@gmail.com>
2026-02-19 13:17:51 +0100

    Inicio Combate

---
+++ AJ_Proyecto1/Combate.cs
@@ -0,0 +1,28 @@
+﻿namespace AJ_Proyecto1;
+
+public class Combate
+{
+    
+    //Atributos
+    public Personaje personaje1;
+    public Personaje personaje2;
+    private Random rng = new Random();
+    private Personaje[] iniciativa = [];
+    
+    
+    public void combate(Personaje p1, Personaje p2)
+    {
+        int dado_1 = LanzarDado(p1.fuerza);
+
+
+
+    }
+
+    public int LanzarDado(int atributo)
+    {
+        int modificador = (int)Math.Floor((atributo - 10) / 2.0); //Lo que hace (int)Math.Floor() es truncar hacia abajo
+        int dado = rng.Next(1,21);
+
+        return dado + modificador;
+    }
+}
