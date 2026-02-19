b10425bedd5769bcdd87b2be371f8d0289cba599
Shangajavi <shangajavi@gmail.com>
2026-02-19 11:42:09 +0100

    Prueba de clase y herencia
    He añadido una herencia de Personaje donde están los atributos básicos y luego la clase de Mago que le he añadido 2 hechizos de tipo string (esto es solo una prueba).

--- AJ_Proyecto1/Program.cs
+++ AJ_Proyecto1/Program.cs
@@ -1,9 +1,12 @@
 ﻿namespace AJ_Proyecto1;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Mago MagoTest = new Mago("Jorge",1,2,3,4,5,"Bolas de Fuego","Bolas Negras");
+        Console.WriteLine("I cast: ");
+        Console.WriteLine(MagoTest.hechizo2);
     }
 }
