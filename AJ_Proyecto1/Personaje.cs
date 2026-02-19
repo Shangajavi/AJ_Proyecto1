255f6a4b5ac58647a833c602659f9522e2c5e9a0
Shangajavi <shangajavi@gmail.com>
2026-02-19 13:17:54 +0100

    Destreza

--- AJ_Proyecto1/Personaje.cs
+++ AJ_Proyecto1/Personaje.cs
@@ -5,37 +5,21 @@ public class Personaje  //Aquí introduzco los atributos básicos de cada person
     public string nombre;   //Por si quieres introducir un nombre
     
     // Estás ya son las stats
+    public int destreza;
     public int fuerza;
     public int constitucion;
     public int inteligencia;
     public int sabiduria;
     public int carisma;
 
-    public Personaje(string nombre, int fuerza, int constitucion, int inteligencia, int sabiduria, int carisma)
+    public Personaje(string nombre,int destreza, int fuerza, int constitucion, int inteligencia, int sabiduria, int carisma)
     {
         this.nombre = nombre;
+        this.destreza = destreza;
         this.fuerza = fuerza;
         this.constitucion = constitucion;
         this.inteligencia = inteligencia;
         this.sabiduria = sabiduria;
         this.carisma = carisma;
     }
-}
-
-
-public class Mago : Personaje //Estoy probando con la primera clase, que he llamado el mago...
-{
-    // En teoría un mago tiene un total de 6 hechizos de nivel un, pero para esta prueba solo le voy a dar 2
-    public string hechizo1;
-    public string hechizo2;
-    
-    //No estoy seguro si esto es correcto pero considero que esto está ma o menos bien...
-    public Mago(string nombre, int fuerza, int constitucion, int inteligencia, int sabiduria, int carisma,
-        string hechizo1, string hechizo2) : base(nombre,fuerza,constitucion, inteligencia, sabiduria, carisma)
-    {
-      this.hechizo1 = hechizo1;
-      this.hechizo2 = hechizo2;
-    }
-
-
 }
--- AJ_Proyecto1/Program.cs
+++ AJ_Proyecto1/Program.cs
@@ -5,7 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Mago MagoTest = new Mago("Jorge",1,2,3,4,5,"Bolas de Fuego","Bolas Negras");
+        Mago MagoTest = new Mago("Jorge",1,1,2,3,4,5, 
+            "Bolas de Fuego","Bolas Negras");
         Console.WriteLine("I cast: ");
         Console.WriteLine(MagoTest.hechizo2);
     }
