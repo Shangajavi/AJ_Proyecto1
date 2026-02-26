83cf8203a873c1937c70a658189bad557499cf25
Shangajavi <shangajavi@gmail.com>
2026-02-26 14:24:43 +0100

    ataques de cada

---
+++ AJ_Proyecto1/Ataque.cs
@@ -0,0 +1,18 @@
+﻿namespace AJ_Proyecto1;
+
+public class Ataque
+{
+    public string nombreAtaque;
+    public int caraDedados;
+    public int atributo;
+    
+    public Ataque(string nombreAtaque,int caraDedados, int atributo)
+    {
+        this.nombreAtaque = nombreAtaque;
+        this.caraDedados = caraDedados;
+        this.atributo = atributo;
+        
+        
+    }
+    
+}
