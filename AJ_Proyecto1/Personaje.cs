e52aefeaa20582d6e1bf6ee8e4628b725a68b1f7
Shangajavi <shangajavi@gmail.com>
2026-03-02 15:51:28 +0100

    Personaje changes

--- AJ_Proyecto1/Personaje.cs
+++ AJ_Proyecto1/Personaje.cs
@@ -15,10 +15,32 @@ public class Personaje  //Aquí introduzco los atributos básicos de cada person
     public int vida = 8;
     public int iniciativa;
     public bool player;
+    public int maxPosicionX;
+    public int maxPosicionY;
+    private bool isSomeoneHere;
+    List<Ataque> ataques;
 
-    public Personaje(string nombre,bool player=false)
+
+    public Personaje(string nombre, bool player = false)
     {
+        
         this.nombre = nombre;
         this.player = player;
+        //Posicion de x e y para ver donde está cada uno de ellos.
+        this.maxPosicionX = maxPosicionX;
+        this.maxPosicionY = maxPosicionY;
+        //Con esto sacas una posicion random de x y de Y.
+        Random rndX = new Random();
+        
+        int posicionX = rndX.Next(1,1 + maxPosicionX);
+        int posicionY = rndX.Next(1,1 + maxPosicionY);
+        
     }
+
+
+
+    
+    
+
+
 }
